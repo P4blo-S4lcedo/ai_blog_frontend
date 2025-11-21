1c152b1eadc293ddb64b9d2bfd5735e24f7d7f18
P4bloS4lcedo <juanpas2006.jpsp@gmail.com>
2025-11-21 12:00:20 -0500

    Confirguración de nombre proyecto en vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ai_blog_fronted/',
+  base: '/ai_blog_frontend/',
   build: {
     outDir: 'docs',
     emptyOutDir: true,
